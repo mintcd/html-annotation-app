0bcdb8945fa5abb48126ced793b58bdf6d05a742
Đăng Minh <dangminh2208@gmail.com>
2026-07-20 13:28:17 +0200

    Convert hooks to submodule

--- hooks/index.ts
+++
@@ -1,6 +0,0 @@
-export { useClient } from "./useClient";
-export { useCoarsePointer, useMobile } from "./useMobile";
-export { useHotkey } from "./useHotkey";
-export { useElementWidth } from "./useElementWidth";
-export { useClickOutside } from "./useClickOutside";
-export { useDebouncedCallback } from "./useDebouncedCallback";
--- hooks/useClient.ts
+++
@@ -1,9 +0,0 @@
-import { useSyncExternalStore } from "react";
-
-const subscribe = () => () => undefined;
-const getClientSnapshot = () => true;
-const getServerSnapshot = () => false;
-
-export function useClient() {
-  return useSyncExternalStore(subscribe, getClientSnapshot, getServerSnapshot);
-}
